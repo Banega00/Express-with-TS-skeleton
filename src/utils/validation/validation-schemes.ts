551ec2d83de9a2da883b3a3ff98baece16c3309b
Branislav Stojanović <branislav.stojanovic@blinking.id>
2022-03-01 14:28:01 +0100

    Joi fix

--- src/utils/validation/validation-schemes.ts
+++ src/utils/validation/validation-schemes.ts
@@ -1,20 +1,20 @@
-import { boolean, date, number, object, string } from 'joi';
+import Joi from 'joi';
 
 //Schemes is JS Object which keys are routes
 //and values are JOI schema objects
 
 export const Schemes = {
-    "/route1": object({
-        message: string().required(),
-        eventObject: object().keys({
-            stepId: number().required(),
-            startTimestamp: date().optional(),
-            endTimestamp: date().optional(),
-            title: string().optional(),
-            isSuccessful: boolean().optional(),
-            business: number().optional(),
-            videoOperatorUserName: string().optional(),
-            sessionId: string().optional()
+    "/route1": Joi.object({
+        message: Joi.string().required(),
+        eventObject: Joi.object().keys({
+            stepId: Joi.number().required(),
+            startTimestamp: Joi.date().optional(),
+            endTimestamp: Joi.date().optional(),
+            title: Joi.string().optional(),
+            isSuccessful: Joi.boolean().optional(),
+            business: Joi.number().optional(),
+            videoOperatorUserName: Joi.string().optional(),
+            sessionId: Joi.string().optional()
         }).required()
     }),
 }
